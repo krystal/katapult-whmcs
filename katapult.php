646365a9d946d7f6019b26d327002ffa3dd7c865
Josh Bonfield <josh@grizzlyware.com>
2021-03-15 12:32:49 +0000

    wip: empty provisioning module

---
+++ katapult.php
@@ -0,0 +1,19 @@
+<?php
+
+if (!defined('WHMCS')) {
+	die('This file cannot be accessed directly');
+}
+
+function katapult_MetaData()
+{
+	return [
+		'DisplayName' => 'Katapult',
+		'RequiresServer' => true
+	];
+}
+
+function katapult_ConfigOptions()
+{
+	//
+}
+
