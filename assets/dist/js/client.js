12b41c728383dcdddd769a17b47f1ca8137e176e
Josh Bonfield <josh@grizzlyware.com>
2021-03-17 15:04:57 +0000

    feat: disable client actions with certain states

--- assets/dist/js/client.js
+++ assets/dist/js/client.js
@@ -1,39 +1,80 @@
 if(typeof kvmService === 'object') {
     (function(vmService) {
 
-        const passwordEl = document.getElementById('kvm_password')
-        const passwordToggleEl = document.getElementById('kvm_password_toggle')
-        const revealDuration = 15;
+        let passwordToggling = () => {
+            const passwordEl = document.getElementById('kvm_password')
+            const passwordToggleEl = document.getElementById('kvm_password_toggle')
+            const revealDuration = 15;
 
-        const setDefaultRevealButton = () => {
-            passwordToggleEl.innerText = 'Reveal for ' + revealDuration + 's';
-            passwordToggleEl.disabled = false
-        }
+            const setDefaultRevealButton = () => {
+                passwordToggleEl.innerText = 'Reveal for ' + revealDuration + 's';
+                passwordToggleEl.disabled = false
+            }
 
-        setDefaultRevealButton();
+            setDefaultRevealButton();
 
-        passwordToggleEl.addEventListener('click', () => {
+            passwordToggleEl.addEventListener('click', () => {
 
-            let timeRemaining = revealDuration;
+                let timeRemaining = revealDuration;
 
-            let handleShowingPassword = () => {
-                timeRemaining--;
+                let handleShowingPassword = () => {
+                    timeRemaining--;
 
-                if (timeRemaining < 1) {
-                    passwordEl.type = 'password';
-                    clearInterval(countdownInterval);
-                    setDefaultRevealButton();
-                } else {
-                    passwordEl.type = 'text'
-                    passwordToggleEl.innerText = 'Hiding in ' + timeRemaining + 's'
-                    passwordToggleEl.disabled = true
+                    if (timeRemaining < 1) {
+                        passwordEl.type = 'password';
+                        clearInterval(countdownInterval);
+                        setDefaultRevealButton();
+                    } else {
+                        passwordEl.type = 'text'
+                        passwordToggleEl.innerText = 'Hiding in ' + timeRemaining + 's'
+                        passwordToggleEl.disabled = true
+                    }
+                };
+
+                handleShowingPassword();
+
+                let countdownInterval = setInterval(handleShowingPassword, 1000);
+            });
+        };
+
+        let disableElements = () => {
+            let elementsToDisable = [];
+
+            switch(vmService.vm.state) {
+                case 'started':
+                    elementsToDisable.push(document.getElementById('Primary_Sidebar-Service_Details_Actions-Custom_Module_Button_Start_VM'))
+                    break;
+
+                case 'stopped':
+                    elementsToDisable.push(document.getElementById('Primary_Sidebar-Service_Details_Actions-Custom_Module_Button_Stop_VM'))
+                    elementsToDisable.push(document.getElementById('Primary_Sidebar-Service_Details_Actions-Custom_Module_Button_Reset_VM'))
+                    break;
+
+                case 'building':
+                    elementsToDisable.push(document.getElementById('Primary_Sidebar-Service_Details_Actions-Custom_Module_Button_Shutdown_VM'))
+                    elementsToDisable.push(document.getElementById('Primary_Sidebar-Service_Details_Actions-Custom_Module_Button_Stop_VM'))
+                    elementsToDisable.push(document.getElementById('Primary_Sidebar-Service_Details_Actions-Custom_Module_Button_Start_VM'))
+                    elementsToDisable.push(document.getElementById('Primary_Sidebar-Service_Details_Actions-Custom_Module_Button_Reset_VM'))
+                    break;
+            }
+
+            // Remove any which weren't found
+            elementsToDisable = elementsToDisable.filter(element => {
+                return !!element;
+            })
+
+            elementsToDisable.forEach(element => {
+                element.disabled = true;
+                element.classList.add('k_disabled')
+
+                if (element.tagName == 'A') {
+                    element.href = 'javascript:void(0)'
                 }
-            };
+            })
+        };
 
-            handleShowingPassword();
-
-            let countdownInterval = setInterval(handleShowingPassword, 1000);
-        });
+        disableElements();
+        passwordToggling();
 
     })(kvmService);
 }
