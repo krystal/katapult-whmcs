8ccb453f6edbddd206fc0fece5a2a3cb33089158
Josh Bonfield <josh@grizzlyware.com>
2021-03-18 12:53:45 +0000

    feat: confirm user actions on VM controls in the admin area

--- assets/dist/js/admin.js
+++ assets/dist/js/admin.js
@@ -2,6 +2,24 @@ if(typeof katapultVmService === 'object') {
     (function(vmService) {
 
         /**
+         * This is fetching all of the action buttons which are available in the client area, for use later in this file
+         */
+        let actionElements = [];
+
+        const fetchActionElements = () => {
+            actionElements = [];
+
+            actionElements.push(document.getElementById('btnStop_VM'))
+            actionElements.push(document.getElementById('btnReset_VM'))
+            actionElements.push(document.getElementById('btnShutdown_VM'))
+            actionElements.push(document.getElementById('btnStart_VM'))
+
+            actionElements = actionElements.filter(element => {
+                return !!element;
+            })
+        }
+
+        /**
          * This function will disable certain buttons in the admin area when the VM is in certain states. For example, not allowing it to be started when it's already started
          */
         let disableElements = () => {
@@ -41,8 +59,38 @@ if(typeof katapultVmService === 'object') {
             })
         };
 
-        // Actually disable the elements
+        /**
+         * This will add listeners to the action buttons to confirm the user wishes to proceed. By default, WHMCS wouldn't question the user when they ask to reset their VM. This stops those accidents.
+         */
+        const confirmUserActions = () => {
+            actionElements.forEach(element => {
+
+                if (element.classList.contains('katapult_confirmed')) {
+                    return;
+                }
+
+                let originalOnClick = element.onclick;
+
+                element.onclick = () => {
+                    confirm('Are you sure?') && originalOnClick()
+                }
+
+                element.classList.add('katapult_confirmed')
+            })
+        };
+
+        // Run those functions
+        fetchActionElements();
         disableElements();
+        confirmUserActions();
+
+        // This fires when the page changes, which WHMCS does when a module command completes. We have to then rebind/re-modify the page as we need.
+        document.getElementById('servicecontent').addEventListener("DOMNodeInserted", () => {
+            fetchActionElements();
+            confirmUserActions();
+            //disableElements(); // Disabling elements isn't enforced here, because the VM might not of transitioned to the new state yet, and it's frustrating to have 'start' greyed out when you just stopped a VM.
+        }, false);
+
     })(katapultVmService);
 }
 
